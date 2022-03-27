a37f5964cbf1f1ce765cf6ce41a73b3933ab980f
Dario Turin <dturin1997@gmail.com>
2022-03-27 17:54:53 -0500

    Initial commit

---
+++ app/src/main/java/com/turin/pem_lab_semana2_01/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.turin.pem_lab_semana2_01
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
